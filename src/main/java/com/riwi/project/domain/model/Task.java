412248db38403dc8a4a2968efb7a3042bd15ee38
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-03 16:54:08 -0500

    Add entity task

---
+++ src/main/java/com/riwi/project/domain/model/Task.java
@@ -0,0 +1,26 @@
+package com.riwi.project.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+
+    private User user;
+
+    private Project project;
+}
