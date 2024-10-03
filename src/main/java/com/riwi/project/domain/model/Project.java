e5bf4833e3a3ac1757774caa8f49e5a965efe4b8
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-03 16:51:18 -0500

    Add entity project

---
+++ src/main/java/com/riwi/project/domain/model/Project.java
@@ -0,0 +1,23 @@
+package com.riwi.project.domain.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+}
