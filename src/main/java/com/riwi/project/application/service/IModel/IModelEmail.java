30773c3973bcf39b1db9a43a5f10f959ba4044c4
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-04 16:26:12 -0500

    create EmailService

--- src/main/java/com/riwi/project/application/service/IModel/IModelEmail.java
+++ src/main/java/com/riwi/project/application/service/IModel/IModelEmail.java
@@ -3,6 +3,5 @@ package com.riwi.project.application.service.IModel;
 import sendinblue.ApiException;
 
 public interface IModelEmail {
-  oid sendEmail(String toEmail, String subject, String content) throws ApiException;
-
+  void sendEmail(String toEmail, String subject, String content) throws ApiException;
 }
