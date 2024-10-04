f83a1460cc75eccadb7f149ec0a23d07f5a96f55
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-04 16:04:24 -0500

    create login dtos

---
+++ src/main/java/com/riwi/project/application/dto/response/LoginResponseDTO.java
@@ -0,0 +1,12 @@
+package com.riwi.project.application.dto.response;
+
+import com.riwi.project.utils.enu.Role;
+
+public class LoginResponseDTO {
+
+    private String message;
+
+    private Role role;
+
+    private String token;
+}
