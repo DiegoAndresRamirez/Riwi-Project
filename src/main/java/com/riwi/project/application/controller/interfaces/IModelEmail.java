46ea900a1aa0468f217cc7087f4c10d3e44f0e53
Diego Ramirez <diegomejiasobsu@gmail.com>
2024-10-04 16:28:49 -0500

    create the imodel to emailController

--- src/main/java/com/riwi/project/application/controller/interfaces/IModelEmail.java
+++ src/main/java/com/riwi/project/application/controller/interfaces/IModelEmail.java
@@ -1,5 +1,10 @@
 package com.riwi.project.application.controller.interfaces;
 
+import org.springframework.http.ResponseEntity;
+
+import com.riwi.project.application.dto.request.EmailRequest;
+
 public interface IModelEmail {
+  ResponseEntity<String> sendEmail(EmailRequest request);
 
 }
