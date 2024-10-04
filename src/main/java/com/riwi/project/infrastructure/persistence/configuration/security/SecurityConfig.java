e73bcc12ca027176ee97612f5b9f4f0277156a75
alejandroava <alejandroalzate.ava@gmail.com>
2024-10-04 17:02:43 -0500

    create jwt complete return in login

--- src/main/java/com/riwi/project/infrastructure/persistence/configuration/security/SecurityConfig.java
+++ src/main/java/com/riwi/project/infrastructure/persistence/configuration/security/SecurityConfig.java
@@ -1,4 +1,35 @@
 package com.riwi.project.infrastructure.persistence.configuration.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/login","/register-user","/register-admin").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+
+    }
 }
